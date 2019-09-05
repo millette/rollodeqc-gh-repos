dabef81fa75c62a6cb1ac33fa26f7fc5f78eb9ab
Robin Millette <robin@millette.info>
2019-09-05 15:01:25 -0400

    Timeout tests after 6 minutes.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import test from 'ava'
 import fn from './'
 
 test('title', async t => {
-  t.timeout(300000)
+  t.timeout(360000)
   const result = await fn('millette')
   t.truthy(result.length > 20)
   t.is(result[0].owner.login, 'millette')
